d8e6aec74965af68959f6dc1ae0a91015d9ed4c6
wenyzbw <1224995167@qq.com>
2019-06-04 19:37:29 +0800

    修改2
    修改2

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//修改密码请改此处
-    if(userName=="xiaojing" &&  pwd=="happybirthday"){
+    if(userName=="陈景卉" &&  pwd=="生日快乐"){
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
